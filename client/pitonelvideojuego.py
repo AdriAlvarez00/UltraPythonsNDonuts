54106f4a3f45ab7d4e9e36bb09080e18cfd87683
AdriAlvarez00 <adralv01@ucm.es>
2021-06-15 17:34:06 +0200

    Refactor de logica interna
    Co-Authored-By: Andrés <47799692+AndresRuizB@users.noreply.github.com>

--- client/pitonelvideojuego.py
+++ client/pitonelvideojuego.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
 import pygame
 import sys
 import random
 import snake_pb2 as serialMsg
+from pygame.math import Vector2
+
+# el tamaño de la ventana y el gridsize tienen que ser divisible, y de resultado un n par, si no se mama (hay que añadir excepciones y tal)
+screen_width = 480
+screen_height = 480
+
+
+#TODO tener un tam de arena independiente para tener UI por fuera o algo asi
+GRID_SIZE = 20
+TILE_SIZE = 24
+
 GAME_SPEED = 5  # esto determina la velocidad del juego (mayor -> mas rapido)
 
 COLOR_SNAKE = (50, 100, 20)
@@ -19,25 +31,19 @@ LENGTH__PER_FOOD = 2
 COLOR_SCORE = (0, 0, 0)
 SIZE_SCORE = 100
 
-# el tamaño de la ventana y el gridsize tienen que ser divisible, y de resultado un n par, si no se mama (hay que añadir excepciones y tal)
-screen_width = 480
-screen_height = 480
 
-gridsize = 20
-grid_width = screen_width/gridsize
-grid_height = screen_height/gridsize
 
 # direcciones usadas para el movimiento de la serpiente
-up = (0, -1)
-down = (0, 1)
-left = (-1, 0)
-right = (1, 0)
+up =  Vector2(0,-1)
+down = Vector2(0,1)
+left = Vector2(-1,0)
+right = Vector2(1,0)
 
 class Snake():
     def __init__(self):
         self.length = SIZE_SNAKE
         # vector de las pociciones del cuerpo -> indice 0 es la cabeza
-        self.positions = [((screen_width/2), (screen_height/2))]
+        self.positions = [Vector2(GRID_SIZE/2,GRID_SIZE/2)]
         # random.choice([up, down, left, right])  #La dir inicial es aleatoria
         self.direction = right
         self.color = COLOR_SNAKE
@@ -49,13 +55,13 @@ class Snake():
     def get_msg(self):
         msg = serialMsg.Snake()
         msg.id = self.id
-        msg.direction.x = self.direction[0]
-        msg.direction.y = self.direction[1]
+        msg.direction.x = self.direction.x
+        msg.direction.y = self.direction.y
         i = 0
         for b in self.positions:
             bodyPos = msg.body.add()
-            bodyPos.x = int(b[0])
-            bodyPos.y = int(b[1])
+            bodyPos.x = int(b.x)
+            bodyPos.y = int(b.y)
             i = i+1
         return msg
     def from_msg(self,msg):
@@ -69,20 +75,18 @@ class Snake():
         return self.positions[0]
 
     def turn(self, dirMov):
-
         # si la serpiente mide mas de 1 cubito, no puede girar hacia atras
-        if self.turned or (self.length > 1 and (dirMov[0] * -1, dirMov[1] * -1) == self.direction):
+        if self.turned or (self.length > 1 and (dirMov.x * -1, dirMov.y * -1) == self.direction):
             return  # tampoco puede girar si no ha caminado desde la ultima vez que giro
         else:  # si podemos, giramos
             self.direction = dirMov
             self.turned = True
 
     def move(self):
-        cur = self.get_head_position()  # pos actual cabeza
-        x, y = self.direction  # direction es una tupla x, y
+        head = self.get_head_position()  # pos actual cabeza
+        dir = self.direction  # direction es una tupla x, y
 
-        new = (((cur[0]+(x*gridsize)) % screen_width),  # nueva pos cabeza en pixeles, haciendo warp al salir de la pantalla
-               (cur[1]+(y*gridsize)) % screen_height)
+        new = Vector2((head.x+dir.x)%GRID_SIZE,(head.y+dir.y)%GRID_SIZE)
 
         # si la posicion nueva esta ya en el cuerpo (nos hemos chocado), reiniciamos el juego
         if new in self.positions[3:]:
@@ -96,7 +100,7 @@ class Snake():
 
     def reset(self):  # reseteamos la serpiente
         self.length = SIZE_SNAKE
-        self.positions = [((screen_width/2), (screen_height/2))]
+        self.positions = [Vector2(GRID_SIZE/2,GRID_SIZE/2)]
         self.direction = right  # random.choice([up, down, left, right])
         self.score = 0
 
@@ -104,9 +108,9 @@ class Snake():
         i = 1
         for p in self.positions:
             # rectangulo que vamos a pintar (pos_x,pos_y, tam_x,tam_y)
-            r = pygame.Rect((p[0], p[1]), (gridsize, gridsize))
+            r = pygame.Rect((p.x*TILE_SIZE, p.y*TILE_SIZE), (TILE_SIZE, TILE_SIZE))
             pygame.draw.rect(surface, self.color, r)  # lo pintamos
-            # efecto de hacer chikita la cola
+            # efecto de hacer chikita la colae
             pygame.draw.rect(surface, COLOR_BG_1, r, 
                             int(8 * i/len(self.positions) + 1))
             i += 1
@@ -140,28 +144,29 @@ class Snake():
 
 class Food():           # Donuts (*/ω＼*)
     def __init__(self):
-        self.position = (0, 0)
+        self.position = Vector2(0, 0)
         self.color = COLOR_FOOD
 
     # ponemos el donut en un lugar aleatorio no ocupado por la serpiente
     def randomize_position(self, snake):
-        self.position = (random.randint(0, grid_width-1)*gridsize,
-                         random.randint(0, grid_height-1)*gridsize)
+        self.position = Vector2(random.randint(0, GRID_SIZE-1),
+                         random.randint(0, GRID_SIZE-1))
 
         # lo unico mas dulce que un donut es la recursion (●'◡'●)
         if self.position in snake.positions:
             self.randomize_position(snake)
 
     def draw(self, surface):
-        r = pygame.Rect((self.position[0]+int(gridsize*0.1), self.position[1] +  # magia para que parezca un donut de verdad
-                         int(gridsize*0.1)), (int(gridsize*0.8), int(gridsize*0.8)))
+        # TODO recuperar los efectos visuales chulos del donut
+        r = pygame.Rect((self.position.x*TILE_SIZE, self.position.y*TILE_SIZE) # magia para que parezca un donut de verdad
+                         , (int(TILE_SIZE*0.8), int(TILE_SIZE*0.8)))
         pygame.draw.rect(surface, self.color, r, SIZE_FOOD)
 
 
 def drawGrid(surface):  # dibujamos el fondo
-    for y in range(0, int(grid_height)):
-        for x in range(0, int(grid_width)):
-            r = pygame.Rect((x*gridsize, y*gridsize), (gridsize, gridsize))
+    for y in range(0, GRID_SIZE):
+        for x in range(0, GRID_SIZE):
+            r = pygame.Rect((x*TILE_SIZE, y*TILE_SIZE), (TILE_SIZE, TILE_SIZE))
 
             if TILE_BG and (x+y) % 2 == 0:  # hacemos el patron de ajedrez
                 pygame.draw.rect(surface, COLOR_BG_1, r)
